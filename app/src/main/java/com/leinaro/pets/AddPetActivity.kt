7709d84a600b770d88d9f04ac86a4266f7149605
nightcodelearning <nightcodelearning@gmail.com>
2018-01-27 21:23:38 -0500

    missing files for first commit

--- app/src/main/java/com/leinaro/pets/AddPetActivity.kt
+++ app/src/main/java/com/leinaro/pets/AddPetActivity.kt
@@ -3,9 +3,6 @@ package com.leinaro.pets
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-
-url = https://Nechis:oraniel91@github.com/Nechis/pets.git
-
 class AddPetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
