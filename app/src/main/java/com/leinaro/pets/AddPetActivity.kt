335ebc7003222556356182199b918ab13cc2299c
nightcodelearning <nightcodelearning@gmail.com>
2018-01-27 21:02:14 -0500

    first commit

---
+++ app/src/main/java/com/leinaro/pets/AddPetActivity.kt
@@ -0,0 +1,16 @@
+package com.leinaro.pets
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+url = https://Nechis:oraniel91@github.com/Nechis/pets.git
+
+class AddPetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pet)
+
+    }
+}
